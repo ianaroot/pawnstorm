acf176270f824ffb94dc00c6f600f3f14a61eea3
IAN ROOT <ianaroot@gmail.com>
2026-04-12 00:40:02 -0400

    autoload presenters

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module RetitledPrincessCarolynProject
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.autoload_paths << Rails.root.join("app/presenters")
+    config.eager_load_paths << Rails.root.join("app/presenters")
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
