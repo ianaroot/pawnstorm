9ed74ff2a099679cb7c14cd1b819dcc879acc8f8
IAN ROOT <ianaroot@gmail.com>
2026-04-16 21:33:30 -0400

    refactor tournament creation to service object

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,63 +1,21 @@
 class TournamentsController < ApplicationController
-  GAMES_PER_PAIR = 10
-
   before_action :authenticate_registered_user!
 
   def new
-    load_selectable_bots
-    @selected_bot_ids = Array(params[:bot_ids]).map(&:to_s)
-    @games_per_pair = GAMES_PER_PAIR
+    creation = Tournaments::CreateTournament.new(user: current_user, params: setup_params)
+    assign_form_state(creation)
   end
 
   def create
-    load_selectable_bots
-    @selected_bot_ids = tournament_params[:entrant_bot_ids].reject(&:blank?)
-    @games_per_pair = parsed_games_per_pair
-    selected_bots = @selectable_bots.select { |bot| @selected_bot_ids.include?(bot.id.to_s) }
-    if selected_bots.length < 2
-      flash.now[:alert] = 'Please choose at least two compiled bots.'
-      render :new, status: :unprocessable_entity
-      return
-    end
-    tournament = nil
-    begin
-      ActiveRecord::Base.transaction do
-        tournament = Tournament.create!(
-          creator: current_user,
-          games_per_pair: @games_per_pair
-        )
+    creation = Tournaments::CreateTournament.new(user: current_user, params: tournament_params)
 
-        tournament_entries_by_bot_id = {}
-        selected_bots.each_with_index do |bot, index|
-          tournament_entries_by_bot_id[bot.id] = tournament.tournament_entries.create!(
-            bot:,
-            bot_owner: bot.user,
-            display_name: bot.name,
-            seed_order: index,
-            compiled_program_snapshot: bot.get_fresh_program
-          )
-        end
-
-        build_match_definitions(selected_bots, games_per_pair: @games_per_pair).shuffle.each do |definition|
-          white_bot = definition.fetch(:white_bot)
-          black_bot = definition.fetch(:black_bot)
-          Match.create!(
-            tournament: tournament,
-            creator: current_user,
-            white_player: white_bot,
-            black_player: black_bot,
-            white_tournament_entry: tournament_entries_by_bot_id.fetch(white_bot.id),
-            black_tournament_entry: tournament_entries_by_bot_id.fetch(black_bot.id)
-          )
-        end
-      end
-    rescue StandardError => e
-      flash.now[:alert] = e.message
+    if creation.call
+      redirect_to tournament_path(creation.tournament), notice: 'Tournament created. Matches are generating now.'
+    else
+      assign_form_state(creation)
+      flash.now[:alert] = creation.error_message
       render :new, status: :unprocessable_entity
-      return
     end
-    tournament.enqueue_next_match!
-    redirect_to tournament_path(tournament), notice: 'Tournament created. Matches are generating now.'
   end
 
   def show
@@ -109,30 +67,17 @@ class TournamentsController < ApplicationController
 
   private
 
-  def load_selectable_bots
-    @selectable_bots = Bot.where(compiled_program_stale: false).where.not(compiled_program: nil).order(:name).includes(:user)
+  def assign_form_state(creation)
+    @selectable_bots = creation.selectable_bots
+    @selected_bot_ids = creation.selected_bot_ids
+    @games_per_pair = creation.games_per_pair
   end
 
-  def build_match_definitions(selected_bots, games_per_pair:)
-    selected_bots.combination(2).flat_map do |bot_a, bot_b|
-      games_per_pair.times.map do |index|
-        if index.even?
-          { white_bot: bot_a, black_bot: bot_b }
-        else
-          { white_bot: bot_b, black_bot: bot_a }
-        end
-      end
-    end
+  def setup_params
+    params.permit(:games_per_pair, bot_ids: [])
   end
 
   def tournament_params
     params.fetch(:tournament, {}).permit(:games_per_pair, entrant_bot_ids: [])
-  end
-
-  def parsed_games_per_pair
-    requested_games = tournament_params[:games_per_pair].to_i
-    return GAMES_PER_PAIR if requested_games <= 0
-
-    requested_games
   end
 end
