3004fda0ecc8b4c6aac2c9fa87b22f6e97f4a0fa
IAN ROOT <ianaroot@gmail.com>
2026-04-27 13:17:54 -0400

    forgotten password flow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "retitled_princess_carolyn_project_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
